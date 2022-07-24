fe20cdad04e3777e4456956fcd68e6c85931f49e
Olga <kartoshkaslukom@gmail.com>
2022-07-24 15:10:59 +0400

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let dict = {};
+  for (i=0; i<bracketsConfig.length; i++){
+    dict[bracketsConfig[i][0]] = bracketsConfig[i][1];
+  }
+  let stack = [];
+    for (i=0; i<str.length; i++) {
+      if (dict[str[i]] === str[i]) {
+          
+        if (stack.includes(str[i]) && stack[stack.length - 1]!== str[i]) {
+          return false
+        } else if (stack[stack.length - 1]=== str[i]) {
+            stack.pop();
+            i++;
+          }
+        }
+      
+      if (str[i] in dict) {
+        stack.push(str[i])
+      } else {
+        if (dict[stack[stack.length - 1]] !== str[i]) {
+          return false
+        } else {
+          stack.pop();
+        }
+      }
+    }
+    
+    return stack.length === 0
+  }
